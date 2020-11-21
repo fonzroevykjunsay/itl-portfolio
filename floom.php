e854a3881eeca63a2b2c3d4c259c74ccb77c7276
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-11-21 23:10:17 +0800

    Add files via upload

---
+++ floom.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+
+<link rel="stylesheet" type="text/css" href="css.css">
+<body>
+
+</body>
+<div class="page">
+  <nav class="navbar">	
+			<img src="fonzlogo.svg" style="width:50px;height:50px;" />    
+    <h1 style="color:#F0FFFF; 
+font-family: sans-serif;">Floom | Same day Flower Delivery | Local Florists </h1>
+
+  </nav>
+    </section>
+  <div class="page-body">
+    <section class="sidebar">
+      <object class="sidebar-item" type="image/svg+xml" data="flowerside1.php" style="width:250px; height:250px;" >
+        <!-- Your fall back here -->
+        <img src="flowerside1.php" />
+      </object>   
+      
+      <object class="sidebar-item" type="image/svg+xml" data="flowerside2.php" style="width:250px; height:250px;" >
+        <!-- Your fall back here -->
+        <img src="flowerside2.php" />
+      </object>
+  
+      </section>
+    <section class="columns">
+        <article class="column-item">
+          <h1 style=" position: relative; color: #ffffff; font-weight: 900px;">What you see is what you get</h1>
+          <i style=" position: relative; width:600px; font-weight: bolder;">Love the bouquet on your screen? Thats exactly what our local florist will prepare
+             freshly for your order or your money back!</i>  </article>
+          <object type="image/svg+xml" data="eye.php" style="opacity: 0.5; margin-top: 25; position: absolute; width:250px; height:180px;" >
+            <!-- Your fall back here -->
+            <img src="eye.php" />
+          </object> 
+      
+             <article class="column-item">  
+              <h1 style=" position: relative; font-weight: bolder;">Always unique, never generic</h1>
+              <i style="width:600px; font-weight: bolder;">We only work with the most talented and unique artisans,
+                 and were passionate about supporting our skilled family of florists.</i></article>
+               <object type="image/svg+xml" data="sun.php" style="opacity: 0.5; margin-left: 30; margin-top: 200; position: absolute; width:190px; height:180px;" >
+              <!-- Your fall back here -->
+              <img src="sun.php" />
+              </object> 
+       
+                     <article class="column-item">
+                     <h1 style=" position: relative; font-weight: bolder;">Hand-delivered with care and attention</h1>
+                      <i style="width:600px; font-weight: bolder;">Each of our orders is professionally arranged, wrapped and 
+                        safely delivered with a hand-written card on the exact day that you need it.</i></article>
+                      <object type="image/svg+xml" data="hand.php" style="opacity: 0.5; margin-left: 10; margin-top: 380; position: absolute; width:190px; height:180px;" >
+                      <!-- Your fall back here -->
+                      <img src="hand.php" />
+                      </object> 
+                         
+    </section>
+  </div>
+</div>
+</div>
+
+
+
+
+
+</head>
+</html>
